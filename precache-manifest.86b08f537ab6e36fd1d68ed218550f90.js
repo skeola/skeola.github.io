8b42d3de32a2921322eea12e6d032f481cd81d9e
skeola <skeola@pdx.edu>
2020-05-19 21:20:18 -0700

    Updates

---
+++ precache-manifest.86b08f537ab6e36fd1d68ed218550f90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6d319fabfaf0b4e70c4aeb245a0de0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1927803c037ba898dce0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f90c3445e92becccad50",
+    "url": "/static/js/2.b6606f9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b6606f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1927803c037ba898dce0",
+    "url": "/static/js/main.401f90ac.chunk.js"
+  },
+  {
+    "revision": "5d7448bc665d7adb0718",
+    "url": "/static/js/runtime-main.c9d04729.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
