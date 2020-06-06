584aefb3832201a07e30f0d8ccbade0afda0464c
skeola <skeola@pdx.edu>
2020-06-05 19:18:20 -0700

    Added photo gallery and changed links to logos instead of text

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-let requestURL = "blog.json";
-let request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
+// Grabs all photos and adds them to the photo div
+let photos = document.getElementById("photos");
+
+const prefix = "photos/img_";
+let imgs = new Array();
+for(i=1; i<=33; i++){
+  let img = document.createElement("img");
+  img.src = prefix.concat(i, ".jpg");
+  img.className = "photo";
+  imgs.push(img);
+}
+console.log(imgs.length);
+
+for(i=0; i<imgs.length; i++){
+  console.log(i);
+  photos.appendChild(imgs[i]);
+}
